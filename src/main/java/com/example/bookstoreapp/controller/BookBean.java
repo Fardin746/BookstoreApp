9772b6e7314cd16c4ae6369e65411d3137f891e7
Muhammad Fardin <118387137+Fardin746@users.noreply.github.com>
2025-11-14 16:35:49 +0530

    Add files via upload

--- src/main/java/com/example/bookstoreapp/controller/BookBean.java
+++ src/main/java/com/example/bookstoreapp/controller/BookBean.java
@@ -1,11 +1,9 @@
 package com.example.bookstoreapp.controller;
 
 
-import com.example.bookstoreapp.exception.BookServiceException;
 import com.example.bookstoreapp.model.Book;
 import com.example.bookstoreapp.service.BookService;
 
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.util.List;
@@ -16,17 +14,13 @@ import java.util.List;
 public class BookBean {
 
     private String header = "";
+    private final BookService service = new BookService();
+    private Book newBook = new Book();
     private boolean editMode;
-    private Book newBook;
-    private String message;
 
-    @EJB
-    private BookService service;
-
-    public boolean getEditMode() {
+    public boolean getEditMode(){
         return editMode;
     }
-
     public boolean isEditMode() {
         return editMode;
     }
@@ -51,85 +45,51 @@ public class BookBean {
         this.newBook = newBook;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-
     public List<Book> getBooks() {
-        try {
-            return service.getBooks();
-        }catch (BookServiceException e){
-            message = e.getMessage();
-           return null;
-        }
+        return service.getBooks();
     }
 
     public String addBook() {
-        try {
-            header = "Add Book";
-            service.addBook(newBook);
-            newBook = new Book();
-            editMode = false;
-            message = null; // clear old error
-            return "bookList.xhtml?faces-redirect=true";
-        } catch (BookServiceException e) {
-            message = e.getMessage();
-            return null;
-        }
+        header = "Add Book";
+        service.addBook(newBook);
+        newBook = new Book();
+        editMode = false;
+        return "bookList.xhtml?faces-redirect=true";
     }
 
     public String editBook(Book book) {
         newBook = book;
         editMode = true;
-        message = null;
         header = "Edit Book";
         return "addBook.xhtml?faces-redirect=true";
     }
 
-    public String updateBook() {
-        try {
-            service.editBook(newBook);
-            message = null;
-            return "bookList.xhtml?faces-redirect=true";
-        } catch (BookServiceException e) {
-            message = e.getMessage();
-            editMode = true;
-            return null;
-        }
+    public String updateBook(){
+        header = "Edit Book";
+        service.editBook(newBook);
+        editMode = false;
+        return  "bookList.xhtml?faces-redirect=true";
     }
 
-    public String deleteBook(int id) {
-        try {
-            service.deleteBook(id);
-            return null;
-        } catch (BookServiceException e) {
-            message = e.getMessage();
-            return null;
-        }
+    public String deleteBook(int id){
+        service.deleteBook(id);
+        return null;
     }
 
 
     // for go to add a book page
     public String addBookPage() {
         header = "Add Book";
-        editMode = false;
         newBook = new Book();
-        message = null;
+        editMode = false;
         return "addBook.xhtml?faces-redirect=true";
     }
 
 
     // for go to the book list page
     public String BookListPage() {
-        message = null;
         return "bookList.xhtml?faces-redirect=true";
     }
 
 
 }
-
--- src/main/java/com/example/bookstoreapp/repository/BookRepository.java
+++ src/main/java/com/example/bookstoreapp/repository/BookRepository.java
@@ -1,20 +1,184 @@
 package com.example.bookstoreapp.repository;
 
-import com.example.bookstoreapp.exception.BookRepositoryException;
+import com.example.bookstoreapp.connection.BookStoreDb;
 import com.example.bookstoreapp.model.Book;
-import javax.ejb.Local;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-@Local
-public interface BookRepository {
 
-    void create(Book book) throws BookRepositoryException;
-
-    List<Book> getAllBooks() throws BookRepositoryException;
-
-    void update(Book book) throws BookRepositoryException;
-
-    void delete(int id)throws BookRepositoryException;
+public class BookRepository {
 
 
+    public List<Book> getAllBooks() {
+        String query = "SELECT * FROM Books";
+        List<Book> books = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = BookStoreDb.getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setName(resultSet.getString("name"));
+                book.setTitle(resultSet.getString("title"));
+                book.setDescription(resultSet.getString("description"));
+                book.setPrice(resultSet.getDouble("price"));
+                book.setType(resultSet.getString("type"));
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return books;
+    }
+
+//    public void create(Book book) {
+//        String query = "INSERT INTO Books (name,title,description,price,type) VALUES (?,?,?,?,?)";
+//        Connection connection = null;
+//        PreparedStatement preparedStatement = null;
+//        try {
+//            connection = BookStoreDb.getConnection();
+//            preparedStatement = connection.prepareStatement(query);
+//            preparedStatement.setString(1, book.getName());
+//            preparedStatement.setString(2, book.getTitle());
+//            preparedStatement.setString(3, book.getDescription());
+//            preparedStatement.setDouble(4, book.getPrice());
+//            preparedStatement.setString(5, book.getType());
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        } finally {
+//            try {
+//                if (preparedStatement != null) {
+//                    preparedStatement.close();
+//                }
+//                if (connection != null) {
+//                    connection.setAutoCommit(true);
+//                    connection.close();
+//                }
+//            } catch (SQLException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//    }
+
+
+    public void delete(int id) {
+        String query = "DELETE FROM Books WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = BookStoreDb.getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    public void update(Book book) {
+        String query = "UPDATE Books SET name = ?,title=?,description=?,price=?,type=? WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = BookStoreDb.getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, book.getName());
+            preparedStatement.setString(2, book.getTitle());
+            preparedStatement.setString(3, book.getDescription());
+            preparedStatement.setDouble(4, book.getPrice());
+            preparedStatement.setString(5, book.getType());
+            preparedStatement.setInt(6, book.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    public void create(Book book) {
+        String query = "INSERT INTO Books (name,title,description,price,type) VALUES (?,?,?,?,?)";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = BookStoreDb.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, book.getName());
+            preparedStatement.setString(2, book.getTitle());
+            preparedStatement.setString(3, book.getDescription());
+            preparedStatement.setDouble(4, book.getPrice());
+            preparedStatement.setString(5, book.getType());
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.setAutoCommit(true);
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+//
 }
